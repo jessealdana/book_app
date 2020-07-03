5b38b7946125397eaf0aa079cc0d1b35359b2376
Jesse Aldana <jesse.aldana@gmail.com>
2020-07-03 15:54:17 -0700

    corrected connection to mongodb

---
+++ book_app/client/build/precache-manifest.5e011853c577ab400c3d3d4fa5df2ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caf686eb4fa2bb97f976",
+    "url": "/static/css/main.00b6c468.chunk.css"
+  },
+  {
+    "revision": "caf686eb4fa2bb97f976",
+    "url": "/static/js/main.d9f0e4b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "e7312ad36a803aa9ea95c0302f2b1b91",
+    "url": "/index.html"
+  }
+];
